575c656d1504f4a7fd3d891e74695730712f5ba4
Alexander Gunin <guninalexander@gmail.com>
2017-10-05 13:08:45 -0700

    fix images

--- src/hello.py
+++ src/hello.py
@@ -1,5 +1,12 @@
 from __future__ import print_function
 import torch
 
-x = torch.Tensor(5, 3)
-print(x)
+if torch.cuda.is_available():
+    print("GPU based")
+    a = torch.LongTensor(10).fill_(3).cuda()
+    print(type(a))
+    print(b = a.cpu())
+else:
+    print("CPU based")
+    x = torch.Tensor(5, 3)
+    print(x)
